c2bfd3b6f3a4230e514e47cc61221bc2e0c38f9b
SAM0926 <ferdinantsamson1002@gmail.com>
2025-11-09 19:06:08 +0530

    3rd push

--- jenkins_project/src/test/java/project_1/ContactTest.java
+++ jenkins_project/src/test/java/project_1/ContactTest.java
@@ -16,6 +16,13 @@ public class ContactTest {
 	}
 	@Test
 	public void modifyContactTest() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser","Chrome");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(PASSWORD);
 		System.out.println("execute modify contact test");
 	}
 
